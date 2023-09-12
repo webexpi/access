2520f9109ee7fec593d06688fb2c546653a0e2ef
Nasandratra Harivonjy <nasandratra.access4@gmail.com>
2023-09-12 08:43:30 -0700

    merge the about page and contact page into one

--- app/about/page.js
+++ app/about/page.js
@@ -2,9 +2,9 @@
 const About = () => (
     <main>
         <h1>About the Author</h1>
-        <p>I'm Nasandratra Harivonjy, but you can call me Nas. I've been immersed in web development for five years, specializing in JavaScript. My favorite tech stack includes React with Node. I remember a time when I was impatient, unfocused, and tried out a few popular programming languages. I dreamt of being admired if I could master them all, but those are common mistakes beginners need to overcome gradually.</p>
+        <p>I'm Nasandratra Harivonjy, but you can call me Nas. I've been immersed in web development for five years, specializing in JavaScript. My favorite tech stack includes React with Node. There was a time when I lacked patience and focus, experimenting with several popular programming languages. I dreamt of being admired if I could master them all, but those are common mistakes beginners need to overcome gradually.</p>
 
-        <p>If I were to advise someone embarking on this journey, I'd recommend focusing on one thing at a time and ensuring you complete at least 70% of a topic before moving on. I'm mostly self-taught, but recently, I decided to complement my skills by studying online to earn a Bachelor's Degree from Brigham Young University Idaho, which should be completed soon. However, self-education is key because you need to stay updated with your chosen languages and engage with the community.</p>
+        <p>If I were to advise someone embarking on this journey, I'd recommend focusing on one thing at a time and ensuring you complete at least 70% of a topic before moving on. I'm mostly self-taught, but recently, I decided to complement my skills by studying online to earn a Bachelor's Degree from Brigham Young University Idaho, which should be completed soon. However, self-education is key because it enables you to stay updated with your chosen languages and engage with the community.</p>
 
         <h2>Missions</h2>
         <p>Here's what DevGasy is all about:</p>
@@ -13,6 +13,9 @@ const About = () => (
             <li>I want to share my insights, research findings, and real-world case studies. My goal is to provide practical, useful information.</li>
             <li>DevGasy simplifies official references into easy-to-understand, enjoyable articles. I aim to make complex concepts straightforward and fun to explore.</li>
         </ul>
+        <h2>Contact</h2>
+        <p>Feel free to reach out to me at devgasy@outlook.com or connect with me on my <a href="https://www.linkedin.com/in/nasandratra-harivonjy" target="blank">LinkedIn profile</a>. I look forward to hearing from you and connecting with fellow enthusiasts.</p>
+
     </main>
 );
 
